2157aa15a7489af7550892ed980f1cab5dc73b96
Swal27 <kprakoso80@gmail.com>
2024-02-09 19:59:59 +0700

    Initial Commit

---
+++ WibuApp/WibuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WibuAppApp.swift
+//  WibuApp
+//
+//  Created by Kukuh on 09/02/24.
+//
+
+import SwiftUI
+
+@main
+struct WibuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
